ad76a5bac5fa88d55e355fd2ae76fd5f68e9c280
simple-alertbox <84765475+simple-alertbox@users.noreply.github.com>
2021-12-27 11:45:19 +0500

    Create f9ca75cc5b.js

---
+++ js/f9ca75cc5b.js
@@ -0,0 +1,1 @@
+window.FontAwesomeKitConfig = {"asyncLoading":{"enabled":false},"autoA11y":{"enabled":true},"baseUrl":"https://ka-f.fontawesome.com","detectConflictsUntil":null,"iconUploads":{},"license":"free","method":"css","minify":{"enabled":true},"token":"f9ca75cc5b","v4FontFaceShim":{"enabled":true},"v4shim":{"enabled":true},"version":"5.15.1"};
